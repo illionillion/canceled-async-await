83f5c05348bbdaae0f9f037ba1eb053851365944
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-02-19 14:21:50 +0900

    await中にsettimeoutの処理を中断はできない...？そもそもasync内で条件分岐でsettimeout走らないようにするしか…

--- sample05/test1.js
+++ sample05/test1.js
@@ -1,27 +1,53 @@
 "use strict";
 
 const sleep = (ss) => { 
-    return new Promise((resolve,reject) => {
-        // const t= setTimeout(
-        setTimeout(
-            () => {
-                // if ((ss/1000)%5==0) {
-                //     reject('5の倍数');
-                //     clearTimeout(t);
-                //     return;
-                // }
-                console.log(ss);
-                resolve();
-            }, 
-        ss);
-    }).catch((v)=>{
-        console.log(v);
+    let timeoutId
+    let r
+    const start = () => new Promise((res) => {
+        r = res
+        timeoutId = setTimeout(() => {
+            timeoutId = null
+            res()
+        }, ss);
     })
+    return{
+        start,
+        cancel: () => {
+            if (timeoutId) {
+                clearTimeout(timeoutId)
+                timeoutId=null
+                r()
+            }
+        }
+    }
+    // return new Promise((resolve,reject) => {
+    //     // const t= setTimeout(
+    //     setTimeout(
+    //         () => {
+    //             // if ((ss/1000)%5==0) {
+    //             //     reject('5の倍数');
+    //             //     clearTimeout(t);
+    //             //     return;
+    //             // }
+    //             console.log(ss);
+    //             resolve();
+    //         }, 
+    //     ss);
+    // }).catch((v)=>{
+    //     console.log(v);
+    // })
 }
 
 (async ()=>{
     for (let i = 1; i <= 10; i++) {
-        await sleep(i*1000);
+        const s = sleep(i*1000)
+        // let cancel =s.cancel
+        await s.start()
+        // if (i%5==0) {
+        //     // console.log(i);
+        //     cancel()
+        // }
+        // await sleep(i*1000);
         console.log(i);
         // sleep(i*1000)
         // .then(()=>{
